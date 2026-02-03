900b7d01eaefaa134903de0bba82d739411089f0
ALeksey Safarov <safarov-ar@yandex.ru>
2026-02-03 20:19:23 +0300

    added movemenet player phisics, added first aid kit

--- src/safarov_otus_hw1/Assets/Code/Player.cs
+++ src/safarov_otus_hw1/Assets/Code/Player.cs
@@ -1,29 +1,52 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 namespace Code
 {
     public class Player : MonoBehaviour
     {
-        [SerializeField] private float speed;
+        [SerializeField] private float Speed;
         // [SerializeField] private float speedH;
-        [SerializeField] private Rigidbody _rigidbody;
+        //[SerializeField] private Rigidbody _rigidbody;
+        private Rigidbody _rigidbody;
+        private int CountFirstAidKit = 0;
+
+        private void Start()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+        }
 
         private void Update()
         {
-            float vel = Input.GetAxis("Vertical");
-            transform.position += Vector3.forward * speed * vel * Time.deltaTime;
-            float rotation = Input.GetAxis("Horizontal");
-            transform.position += Vector3.right * speed * rotation * Time.deltaTime;
+            //float vel = Input.GetAxis("Vertical");
+            //transform.position += Vector3.forward * Speed * vel * Time.deltaTime;
+            //float rotation = Input.GetAxis("Horizontal");
+            //transform.position += Vector3.right * Speed * rotation * Time.deltaTime;
   
             
         }
 
         private void FixedUpdate()
         {
-            // float vel = Input.GetAxis("Vertical");
-            // _rigidbody.linearVelocity = Vector3.forward * speed;
-            // float rotation = Input.GetAxis("Horizontal");
-            // _rigidbody.linearVelocity = Vector3.right * speed * rotation;
+            MovementPlayer();
+        }
+
+        private void MovementPlayer()
+        {
+            float moveV = Input.GetAxis("Vertical");
+            float moveH = Input.GetAxis("Horizontal");
+            Vector3 movement = new Vector3(moveH, 0.0f, moveV);
+            _rigidbody.AddForce(movement * Speed);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.gameObject.CompareTag("FirstAidKit"))
+            {
+                CountFirstAidKit++;
+                Debug.Log($"Count first aid kit = {CountFirstAidKit}");
+                Destroy(collision.gameObject);
+            }
         }
     }
 }
