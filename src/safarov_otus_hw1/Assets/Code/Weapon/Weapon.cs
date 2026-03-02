8e26c2bfbc33ac93c2aea44adb48da9cd4ac9369
sar0868 <safarov-ar@yandex.ru>
2026-03-02 23:31:05 +0300

    add queue, change weapon

--- src/safarov_otus_hw1/Assets/Code/Weapon/Weapon.cs
+++ src/safarov_otus_hw1/Assets/Code/Weapon/Weapon.cs
@@ -2,17 +2,25 @@ using UnityEngine;
 
 namespace Code
 {
+    [RequireComponent(typeof(Collider))]
     public abstract class Weapon : MonoBehaviour
     {
         [SerializeField] protected int _level = 1;
         [SerializeField] protected Transform _barrel;
         [SerializeField] protected WeaponUpgradeData _weaponUpgradeData;
+        private Collider _collider;
 
         private float _shotDelay;
 
         public bool CanShot { get; private set ; }
         public float LastShootTime { get; protected set; }
         protected float Force { get; private set; }
+
+        protected void Awake()
+        {
+            _collider = GetComponent<Collider>();
+            _collider.isTrigger = true;
+        }
 
         protected virtual void Start()
         {
@@ -22,7 +30,7 @@ namespace Code
             Force = data.Force;
             
         }
-        private void Update()
+        protected void Update()
         {
             CanShot = _shotDelay <= LastShootTime;
 
@@ -36,5 +44,20 @@ namespace Code
         public abstract void Fire();
         public abstract void Recharge();
 
+        public void SetActive(bool isActive)
+        {
+            gameObject.SetActive(isActive);
+        }
+
+        protected void OnTriggerEnter(Collider other)
+        {
+            Transform player = GameObject.FindGameObjectWithTag("MainCamera").transform;
+            Transform weaponRoot = player.transform.Find("WeaponsRoot");
+            transform.SetParent(weaponRoot);
+            var weaponController = player.GetComponent<WeaponController>();
+            weaponController.AddWeapon(this);
+            transform.localPosition = Vector3.zero;
+            transform.localRotation = Quaternion.identity;
+        }
     }
 }
--- src/safarov_otus_hw1/Assets/Code/Weapon/WeaponController.cs
+++ src/safarov_otus_hw1/Assets/Code/Weapon/WeaponController.cs
@@ -4,19 +4,44 @@ namespace Code
 {
     public class WeaponController : MonoBehaviour
     {
-        [SerializeField] private Weapon _weapon;
+        private WeaponsSelector _weaponSelector;
+        
+        private void Start() {
+            Weapon[] weapons = GetComponentsInChildren<Weapon>(true);
+            _weaponSelector = new WeaponsSelector(weapons);
+        }
 
         private void Update()
         {
+            SelectWeapon();
+            
             if (Input.GetMouseButton(0))
             {
-                _weapon.Fire();
+                _weaponSelector.Fire();
             }
 
             if (Input.GetKeyDown(KeyCode.Q))
             {
-                _weapon.Recharge();
+                _weaponSelector.Recharge();
             }
+        }
+
+        private void SelectWeapon()
+        {
+            if (Input.GetKeyDown(KeyCode.N))
+            {
+                _weaponSelector.Next();
+            }
+
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                _weaponSelector.Preview();
+            }
+        }
+
+        public void AddWeapon(Weapon weapon)
+        {
+            _weaponSelector.AddWeapon(weapon);
         }
     }
 }
