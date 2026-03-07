fa9ae834c4b3993c5960d3eab11d142b23dae656
sar0868 <safarov-ar@yandex.ru>
2026-03-07 23:12:45 +0300

    added audio mixer

--- src/safarov_otus_hw1/Assets/Code/Weapon/AudioSystem.cs
+++ src/safarov_otus_hw1/Assets/Code/Weapon/AudioSystem.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Audio;
 
 namespace Code
 {
@@ -8,10 +9,13 @@ public sealed class AudioSystem : MonoBehaviour
 {
     [SerializeField] private AudioSource _background;
     [SerializeField] private AudioClip[] _backgroundSounds;
+    [SerializeField] private AudioMixer _audioMixer;
 
     private void Start()
     {
         _background.clip = _backgroundSounds[Random.Range(0, _backgroundSounds.Length)];
         _background.Play();
+        _audioMixer.SetFloat("volume_bg", -20f);
+        _audioMixer.SetFloat("volume_step", 10f);
     }
 }
