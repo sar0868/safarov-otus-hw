ef8de9332d5d10129b6753b888ad3f89bc22ea48
sar0868 <safarov-ar@yandex.ru>
2026-04-19 23:29:23 +0300

    added move

--- src/safarov_otus_hw1/Assets/Code/Player/PlayerMovement.cs
+++ src/safarov_otus_hw1/Assets/Code/Player/PlayerMovement.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 namespace Code
@@ -6,7 +5,7 @@ namespace Code
     [RequireComponent(typeof(CharacterController))]
     public sealed class PlayerMovement : MonoBehaviour
     {
-        // [SerializeField] private InputService _inputService;
+        [SerializeField] private OldInputService _inputService;
         [SerializeField] private float _speed = 10f;
         private CharacterController _characterController;
         private Camera _camera;
@@ -30,10 +29,12 @@ namespace Code
             float move_speed = _speed * Time.deltaTime;
 
             // float xInput = _inputService.move.x * move_speed;
-            float xInput = Input.GetAxis("Horizontal") * move_speed;
+            // float xInput = Input.GetAxis("Horizontal") * move_speed;
+            float xInput = _inputService.GetMoveAxisX() * move_speed;
 
+            float zInput = _inputService.GetMoveAxisZ() * move_speed;
             // float zInput = _inputService.move.y * move_speed;
-            float zInput = Input.GetAxis("Vertical") * move_speed;
+            // float zInput = Input.GetAxis("Vertical") * move_speed;
 
             Vector3 move = new Vector3(xInput, 0, zInput);
             move = Vector3.ClampMagnitude(move, _speed);
@@ -41,10 +42,10 @@ namespace Code
             move = transform.TransformDirection(move);
             // _moveDirection = transform.right * xInput + transform.forward * zInput;
 
-            // if (_inputService.HasMoveInput())
-            // {
-            _characterController.Move(move);
-            // }
+            if (_inputService.HasMoveInput())
+            {
+                _characterController.Move(move);
+            }
         }
 
         private void Look()
