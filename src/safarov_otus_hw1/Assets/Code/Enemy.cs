7d07b2d370f23fa3ccfbef867df86947123c7c8f
sar0868 <safarov-ar@yandex.ru>
2026-04-23 21:15:12 +0300

    added navMesh

--- src/safarov_otus_hw1/Assets/Code/Enemy.cs
+++ src/safarov_otus_hw1/Assets/Code/Enemy.cs
@@ -5,17 +5,23 @@ namespace Code
 {
     public sealed class Enemy : MonoBehaviour
     {
+
+        [SerializeField] private Animator _animator;
         public void ReactToHit()
         {
+
             StartCoroutine(Die());
+            // _animator.SetBool("Death", true);
         }
 
         private IEnumerator Die()
         {
+
             transform.Rotate(-90f, 0f, 0f);
             yield return new WaitForSeconds(1f);
 
-            Destroy(gameObject);
+            gameObject.SetActive(false);
+            // Destroy(gameObject);
         }
     }
 }
