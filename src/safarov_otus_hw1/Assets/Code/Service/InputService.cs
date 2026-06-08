2e2722a6b8f2c0acf70dc146ff5b6f71b1e5bc20
sar0868 <safarov-ar@yandex.ru>
2026-06-08 09:25:55 +0300

    added character behavior

---
+++ src/safarov_otus_hw1/Assets/Code/Service/InputService.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+
+namespace Code.Service
+{
+    public class InputService : MonoBehaviour
+    {
+        public UnityEvent attackEvent = new();
+        public Vector2 move;
+        public Vector2 look;
+
+        private void OnMove(InputValue value)
+        {
+            move = value.Get<Vector2>();
+        }
+
+        private void OnLook(InputValue value)
+        {
+            look = value.Get<Vector2>();
+        }
+
+        private void OnAttack(InputValue value)
+        {
+            attackEvent?.Invoke();
+        }
+    }
+}
